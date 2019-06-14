5c6859a6707332e50c646bec4a78b89af80e8999
Denis Krivak <dokrivak@avito.ru>
2019-06-15 00:24:01 +0300

    Add more tests.

--- feed_test.go
+++ feed_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -35,4 +36,44 @@ func TestGetImage(t *testing.T) {
 			assert.Equal(t, tt.out, getImage(tt.in))
 		})
 	}
+}
+
+func TestFeed(t *testing.T) {
+	item := Item{
+		Title:     "Title",
+		Image:     "https://example.com/0001.png",
+		Published: time.Date(2000, 1, 1, 0, 0, 0, 0, time.UTC),
+	}
+	fetcher := &fetcherMock{item: item}
+
+	f := NewFeed(fetcher, 25*time.Millisecond)
+	go f.Start()
+
+	// Got item for the first time
+	timer := time.NewTimer(50 * time.Millisecond)
+	select {
+	case <-timer.C:
+		t.Fatal("Got no item")
+	case got := <-f.Feed():
+		assert.Equal(t, item, got)
+	}
+
+	// Got the same item again (no output)
+	timer = time.NewTimer(50 * time.Millisecond)
+	select {
+	case <-f.Feed():
+		t.Fatal("Got unexpected item")
+	case <-timer.C:
+	}
+
+	f.Stop()
+}
+
+type fetcherMock struct {
+	item Item
+	err  error
+}
+
+func (f *fetcherMock) Fetch() (Item, error) {
+	return f.item, f.err
 }
